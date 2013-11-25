0e3e50f7eaf5c1d2974e98e2c98ba8983bb181f0
Vladimir Pasquier <vpasquier@nuxeo.com>
2013-11-25 19:38:37 +0100

    NXIDE-273: - Add eclipse project file for each test project - Generate project for each test framework selected - Handle webdriver test project as a new java project with Nuxeo nature

--- plugins/org.nuxeo.ide.sdk543/src/org/nuxeo/ide/sdk/projects/marketplace/MarketplacePackageWizard.java
+++ plugins/org.nuxeo.ide.sdk543/src/org/nuxeo/ide/sdk/projects/marketplace/MarketplacePackageWizard.java
@@ -16,8 +16,18 @@
  */
 package org.nuxeo.ide.sdk.projects.marketplace;
 
+import static org.nuxeo.ide.sdk.projects.marketplace.MartketplaceWizardConstants.FUNKLOAD_ENABLED;
+import static org.nuxeo.ide.sdk.projects.marketplace.MartketplaceWizardConstants.SELENIUM_ENABLED;
+import static org.nuxeo.ide.sdk.projects.marketplace.MartketplaceWizardConstants.WEBDRIVER_ENABLED;
+
+import java.io.File;
+
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.wizard.WizardPage;
 import org.nuxeo.ide.sdk.projects.AbstractProjectWizard;
+import org.nuxeo.ide.sdk.projects.CreateProjectFromTemplate;
+import org.nuxeo.ide.sdk.projects.ProjectTemplateContext;
+import org.nuxeo.ide.sdk.templates.Constants;
 
 /**
  * @since 1.2
@@ -25,6 +35,12 @@ import org.nuxeo.ide.sdk.projects.AbstractProjectWizard;
 public class MarketplacePackageWizard extends AbstractProjectWizard {
 
     public static final String ID = MarketplacePackageWizard.class.getName();
+
+    public static final String WEBDRIVER = "webdriver";
+
+    public static final String SELENIUM = "selenium";
+
+    public static final String FUNKLOAD = "funkload";
 
     public MarketplacePackageWizard() {
         super("marketplace");
@@ -34,6 +50,55 @@ public class MarketplacePackageWizard extends AbstractProjectWizard {
     protected WizardPage[] createPages() {
         return new WizardPage[] { new EclipseProjectPage(),
                 new MarketplaceInformationPage() };
+    }
+
+    /**
+     * @since 1.2 - depending on test framework selection in MP information
+     *        wizard page, generate new test project(s).
+     */
+    @Override
+    protected boolean execute(ProjectTemplateContext ctx)
+            throws JavaModelException {
+        if (!super.execute(ctx)) {
+            return false;
+        }
+        if (Boolean.parseBoolean(ctx.getProperty(WEBDRIVER_ENABLED))) {
+            ProjectTemplateContext initCtx = (ProjectTemplateContext) ctx.clone();
+            generateProjectFromTemplate(ctx, WEBDRIVER);
+            ctx = initCtx;
+        }
+        if (Boolean.parseBoolean(ctx.getProperty(SELENIUM_ENABLED))) {
+            ProjectTemplateContext initCtx = (ProjectTemplateContext) ctx.clone();
+            generateProjectFromTemplate(ctx, SELENIUM);
+            ctx = initCtx;
+        }
+        if (Boolean.parseBoolean(ctx.getProperty(FUNKLOAD_ENABLED))) {
+            generateProjectFromTemplate(ctx, FUNKLOAD);
+        }
+        return true;
+    }
+
+    protected boolean generateProjectFromTemplate(ProjectTemplateContext ctx,
+            String frameworkTemplate) {
+        ctx.setTemplate(frameworkTemplate);
+        String projectId = ctx.getProperty(Constants.PROJECT_ID);
+        String projectPkg = ctx.getProperty(Constants.PROJECT_PACKAGE);
+        String artifactName = ctx.getProperty(Constants.ARTIFACT_NAME);
+        String artifactId = ctx.getProperty(Constants.ARTIFACT_ID);
+        String projectName = ctx.getProperty(Constants.PROJECT_NAME);
+        File projectLocation = ctx.getProjectLocation();
+        ctx.setProjectLocation(new File(projectLocation.getParentFile(),
+                projectLocation.getName() + "-" + frameworkTemplate));
+        ctx.put(Constants.PROJECT_ID, projectId + "-" + frameworkTemplate);
+        ctx.put(Constants.PROJECT_NAME, projectName + "-" + frameworkTemplate);
+        ctx.put(Constants.ARTIFACT_NAME, artifactName + "-" + frameworkTemplate);
+        ctx.put(Constants.ARTIFACT_ID, artifactId + "-" + frameworkTemplate);
+        ctx.put(Constants.PROJECT_PACKAGE, projectPkg + "." + frameworkTemplate);
+        CreateProjectFromTemplate op = new CreateProjectFromTemplate(ctx);
+        if (!CreateProjectFromTemplate.run(getShell(), getContainer(), op)) {
+            return false;
+        }
+        return true;
     }
 
 }
