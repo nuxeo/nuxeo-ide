876b90cd26f94b315774185da8d8ecf918f0d5c0
Julien Carsique <jcarsique@nuxeo.com>
2014-12-02 19:43:32 +0100

    NXIDE-340: try to update the properties

--- plugins/org.nuxeo.ide.sdk/src/org/nuxeo/ide/jdt/NuxeoCodeFormatterConfigurationBlock.java
+++ plugins/org.nuxeo.ide.sdk/src/org/nuxeo/ide/jdt/NuxeoCodeFormatterConfigurationBlock.java
@@ -17,10 +17,14 @@
  */
 package org.nuxeo.ide.jdt;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.eclipse.jdt.internal.ui.JavaPlugin;
 import org.eclipse.jdt.internal.ui.preferences.PreferencesAccess;
 import org.eclipse.jdt.internal.ui.preferences.formatter.CodeFormatterConfigurationBlock;
 import org.eclipse.jdt.internal.ui.preferences.formatter.IProfileVersioner;
@@ -42,8 +46,15 @@ public class NuxeoCodeFormatterConfigurationBlock extends CodeFormatterConfigura
 
     protected ProfileManager profileManager;
 
+    protected final IScopeContext fCurrContext;
+
     public NuxeoCodeFormatterConfigurationBlock(IProject project, PreferencesAccess access) {
         super(project, access);
+        if (project != null) {
+            this.fCurrContext = null;
+        } else {
+            this.fCurrContext = access.getInstanceScope();
+        }
     }
 
     @Override
@@ -76,5 +87,51 @@ public class NuxeoCodeFormatterConfigurationBlock extends CodeFormatterConfigura
     @Override
     public ProfileManager getProfileManager() {
         return profileManager;
+    }
+
+    /**
+     * Copied from org.eclipse.jdt.internal.ui.preferences.cleanup.CleanUpConfigurationBlock#preferenceChanged(PreferenceChangeEvent)
+     */
+    @Override
+    protected void preferenceChanged(IEclipsePreferences.PreferenceChangeEvent event) {
+        if ("org.eclipse.jdt.ui.formatterprofiles".equals(event.getKey())) {
+            try {
+                String id = this.fCurrContext.getNode("org.eclipse.jdt.ui").get("formatter_profile", null);
+                if (id == null) {
+                    this.profileManager.getDefaultProfile().getID();
+                }
+                List<ProfileManager.Profile> oldProfiles = this.profileManager.getSortedProfiles();
+                ProfileManager.Profile[] oldProfilesArray = oldProfiles.toArray(new ProfileManager.Profile[oldProfiles.size()]);
+                for (int i = 0; i < oldProfilesArray.length; i++) {
+                    if ((oldProfilesArray[i] instanceof ProfileManager.CustomProfile)) {
+                        this.profileManager.deleteProfile((ProfileManager.CustomProfile) oldProfilesArray[i]);
+                    }
+                }
+
+                List<ProfileManager.Profile> newProfiles = this.profileStore.readProfilesFromString((String) event.getNewValue());
+                for (Iterator<ProfileManager.Profile> iterator = newProfiles.iterator(); iterator.hasNext();) {
+                    ProfileManager.CustomProfile profile = (ProfileManager.CustomProfile) iterator.next();
+                    this.profileManager.addProfile(profile);
+                }
+
+                ProfileManager.Profile profile = this.profileManager.getProfile(id);
+                if (profile != null) {
+                    this.profileManager.setSelected(profile);
+                } else {
+                    this.profileManager.setSelected(this.profileManager.getDefaultProfile());
+                }
+            } catch (CoreException e) {
+                JavaPlugin.log(e);
+            }
+        } else if ("formatter_profile".equals(event.getKey())) {
+            if (event.getNewValue() == null) {
+                this.profileManager.setSelected(this.profileManager.getDefaultProfile());
+            } else {
+                ProfileManager.Profile profile = this.profileManager.getProfile((String) event.getNewValue());
+                if (profile != null) {
+                    this.profileManager.setSelected(profile);
+                }
+            }
+        }
     }
 }
